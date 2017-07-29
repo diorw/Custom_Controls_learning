d32665eac8db991f221c97408a39d614a3ca5459
Wu Di Ao <406714601@qq.com>
2017-07-29 22:48:53 +0800

    粘性拉动自定义属性

--- app/src/main/java/com/example/wda/CustomControlsLearning/TouchPullView.java
+++ app/src/main/java/com/example/wda/CustomControlsLearning/TouchPullView.java
@@ -3,6 +3,7 @@ package com.example.wda.CustomControlsLearning;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +17,13 @@ public class TouchPullView extends View {
     private float mCirclePointX,mCirclePointY;
     private float mProgress;
     private int mDragHeight = 800;
+    private int mtargetWidth;
+    private Path mPath = new Path();
+    private Paint mPaint;
+    //控制控制点的Y坐标
+    private int mTargetGravity;
+    //角度变换 0-135
+    private int mTangentAngle = 120;
     public TouchPullView(Context context) {
         super(context);
         init();
@@ -46,6 +54,15 @@ public class TouchPullView extends View {
         p.setColor(0xFF000000);
         mCirclePaint = p;
 
+        p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        //抗锯齿
+        p.setAntiAlias(true);
+        //防抖动
+        p.setDither(true);
+        //填充
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(0xFF000000);
+        mPaint = p;
     }
 
     @Override
@@ -86,8 +103,7 @@ public class TouchPullView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mCirclePointX = getWidth()>>1;
-        mCirclePointY = getHeight()>>1;
+        updatePathLayout();
     }
 
 
@@ -95,8 +111,11 @@ public class TouchPullView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        //画贝塞尔曲线
+        canvas.drawPath(mPath,mPaint);
 
 
+        //画圆
         canvas.drawCircle(mCirclePointX,
                 mCirclePointY,
                 mCircleRadius,
@@ -107,5 +126,21 @@ public class TouchPullView extends View {
         mProgress=progress;
         //请求重新进行测量
         requestLayout();
+    }
+
+    private void updatePathLayout(){
+        final float progress = mProgress;
+        final Path path = mPath;
+        path.reset();
+        final float w = getValueByLine(getWidth(),mtargetWidth,progress);
+        final float h = getValueByLine(0,mDragHeight,progress);
+        final float cPointX = w/2;
+        final float cRadius = mCircleRadius;
+        final float cPointY = h-cRadius;
+        final float endControlY = mTargetGravity;
+
+    }
+    private float getValueByLine(float start,float end,float Progress){
+        return start+(end-start)*Progress;
     }
 }
